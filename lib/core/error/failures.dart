946e8f30fca1d12bf522847b3f8731e730675da6
Carmine Toriello <carminetoriello95@gmail.com>
2020-06-26 17:49:08 +0200

    Commit datasource contracts

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -1,5 +1,18 @@
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
-  Failure([List propertie = const <dynamic>[]]); 
+  Failure([List properties = const <dynamic>[]]); 
+}
+
+// General failures
+class ServerFailure extends Failure{
+  @override
+  // TODO: implement props
+  List<Object> get props => null;
+}
+
+class CacheFailure extends Failure{
+  @override
+  // TODO: implement props
+  List<Object> get props => null;
 }
